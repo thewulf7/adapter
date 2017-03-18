9eac99946b67cdeef39ccfab9e4fd0fd908cf0c4
UsherBaby <io.2016@outlook.com>
2017-03-18 15:23:13 +0800

    update gradle

--- PacificAdapter/demo/src/main/java/com/example/demo/MainActivity.java
+++ PacificAdapter/demo/src/main/java/com/example/demo/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
